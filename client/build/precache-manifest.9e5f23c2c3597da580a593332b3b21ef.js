0ab643e27142a70918a02312e3aa0b62afa7d47f
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-05 12:23:54 +0200

    Add auth flow

---
+++ client/build/precache-manifest.9e5f23c2c3597da580a593332b3b21ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd5c4fae6dbe1e9b552209e5d433043",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c653edf3a0b0660d51",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d9d17ff392ab456daa27",
+    "url": "/static/js/2.9dab47d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9dab47d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32c653edf3a0b0660d51",
+    "url": "/static/js/main.d93884fe.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => console.log('Error on start: ' + err.stack));
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({test: 'new deploy'});
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+const authRoutes = require('./routes/authRoutes');
+authRoutes(app);
+// mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
