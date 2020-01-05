194c395bec53c812e254ae1ffd9e2ce11c312a89
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-05 13:01:28 +0200

    Add client size

--- services/passport.js
+++ services/passport.js
@@ -23,17 +23,14 @@ passport.use(new GoogleStrategy({
         clientSecret: keys.googleClientSecret,
         callbackURL: "/auth/google/callback",
     },
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id})
-            .then((existingUser) => {
-                if (existingUser) {
-                    done(null, existingUser);
-                } else {
-                    new User({googleId: profile.id,})
-                        .save()
-                        .then(user => done(null, user));
-                }
-                console.log('SUCCESS');
-            });
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id });
+
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
     }
 ));
