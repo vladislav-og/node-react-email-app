c662ebde22f990d760dbf0d8f6a0a7a37a8fc841
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-11 19:03:40 +0200

    Add credits to user model and manage stripe post request

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const keys = require('./config/keys');
+const bodyParser = require('body-parser');
+
 require('./models/User');
 require('./services/passport');
 
@@ -16,7 +18,7 @@ mongoose.connect(keys.mongoURI, {
     .catch(err => console.log('Error on start: ' + err.stack));
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -29,7 +31,10 @@ app.use(passport.session());
 
 
 const authRoutes = require('./routes/authRoutes');
+const billingRoutes = require('./routes/billingRoutes');
 authRoutes(app);
+billingRoutes(app);
+
 // mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema; // or const { Schema } = mongoose;
 
 const userSchema = new Schema({
     googleId: String,
+    credits: {type: Number, default: 0}
 });
 
 mongoose.model('users', userSchema);
