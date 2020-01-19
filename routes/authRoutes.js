6c60f89d746aa0af638eb1444ce3469731e4046b
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-19 18:07:54 +0200

    Delete useless route from Express

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,6 @@
 const passport = require('passport');
 
 module.exports = (app) => {
-    app.get('/', (req, res) => {
-        res.send({test: 'just changed in life mode'});
-    });
-
     app.get(
         '/auth/google',
         passport.authenticate('google', {
