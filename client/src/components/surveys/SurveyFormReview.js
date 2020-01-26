81d168e973dd66239e732d56e3c9509f9e337db5
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-26 15:34:10 +0200

    Add survey sending UI

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { connect } from "react-redux";
 import formFields from './formFields';
 import * as actions from "../../actions"
+import { withRouter } from 'react-router-dom';
 
-const SurveyFormReview = ({ onCancel, formValues, submitSurvey}) => {
+const SurveyFormReview = ({ onCancel, formValues, submitSurvey, history}) => {
     const reviewField = formFields.map(({label, name}) => {
         return (
             <div key={name}>
@@ -23,7 +24,7 @@ const SurveyFormReview = ({ onCancel, formValues, submitSurvey}) => {
                 Go Back
                 <i className="material-icons right">cancel</i>
             </button>
-            <button className="green btn-flat right white-text" onClick={() => submitSurvey(formValues)}>
+            <button className="green btn-flat right white-text" onClick={() => submitSurvey(formValues, history)}>
                 Send Survey
                 <i className="material-icons right">email</i>
             </button>
@@ -36,4 +37,4 @@ function mapStateToProps(state) {
         formValues: state.form.surveyForm.values
     };
 }
-export default connect(mapStateToProps, actions)(SurveyFormReview);
+export default connect(mapStateToProps, actions)(withRouter(SurveyFormReview));
