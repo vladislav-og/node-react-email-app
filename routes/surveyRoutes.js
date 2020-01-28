14f94f99f7d2a9562750e3622227a62ce88a75ab
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-28 22:53:22 +0200

    Add get all surveys route

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,14 +9,21 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
+
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user: req.user.id })
+            .select({ recipients: false});
+        res.send(surveys)
+    });
+
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting');
     });
 
-    app.post('/api/surveys/webhooks', (req, res) => {
+    app.post('/api/surveys/webhooks', async (req, res) => {
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.chain(req.body)
+        _.chain(req.body)
             .map(({ email, url }) => {
 
                 const match = p.test(new URL(url).pathname);
@@ -26,9 +33,20 @@ module.exports = app => {
             })
             .compact()
             .uniqBy('email', 'surveyId')
+            .each(event => {
+                Survey.updateOne({
+                    _id: event.surveyId,
+                    recipients: {
+                        $elemMatch: { email: event.email, responded: false}
+                    }
+                }, {
+                    $inc: { [event.choice]: 1 }, // mongo operator to add 1 to choice
+                    $set: { 'recipients.$.responded': true},
+                    lastResponded: new Date(),
+                }).exec();
+            })
             .value();
 
-        console.log(events);
         res.send({});
     });
 
