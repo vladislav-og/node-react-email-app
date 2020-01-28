d940784724c786234a2084cc97c2faee379d54af
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-01-28 23:24:12 +0200

    Add survelist UI

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import SurveyList from './surveys/SurveyList'
 
 const Dashboard = () => {
   return (
       <div style={{ textAlign: 'center' }}>
-          <h1>
-              Dashboard!
-          </h1>
+          <SurveyList/>
           <div className="fixed-action-btn">
               <Link className="waves-effect btn-floating btn-large red" to="/surveys/new">
                   <i className="material-icons">add</i>
